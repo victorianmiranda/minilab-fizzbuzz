2eee2fbce49d134b2098f4aff305eb6aaabfd412
Victoria Miranda <vitorianevesmiranda@gmail.com>
2022-03-02 12:19:24 -0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-// Seu código aqui!
-
-let count = 1;
-
-while (count <= 100) {
-  if (i % 3 === 0) {
-    console.log("fizz");
-  }
-
-  count++;
+for (let i = 1; i < 101; i++) {
+  if (i % 15 == 0) console.log("FizzBuzz");
+  else if (i % 3 == 0) console.log("Fizz");
+  else if (i % 5 == 0) console.log("Buzz");
+  else console.log(i);
 }
